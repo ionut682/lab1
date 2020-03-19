d9f20c11bbc5521821631815411649b683213bf8
Rasinar Ioan <rasinarionut@yahoo.com>
2020-03-19 18:08:31 +0200

    orice

--- src/loose/oose/fis/documents/Document.java
+++ src/loose/oose/fis/documents/Document.java
@@ -11,7 +11,7 @@ public abstract class Document {
 
     @Override
     public String toString() {
-        String res = "";
+        String res = "bbbbbbbbbbb";
         for (String cuvant : continut) {
             res += cuvant + " ";
         }
